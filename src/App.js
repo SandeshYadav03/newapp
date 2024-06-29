7b0558499593126972b8d777836e85cdf1aa377e
Sandesh Yadav <sandeshyadav5108@gmail.com>
2024-06-29 13:01:05 +0530

    Add form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const fetchNumber = async () => {
+    const response = await fetch(`https://swapi.dev/api/people/${9}/`);
+    return await response.json();
+  };
+
+  const handleAddRecord = async () => {
+    try {
+      const response = await fetchNumber();
+      setData([...data, response]);
+    } catch (error) {
+      console.error('Error fetching random character:', error);
+    }
+  };
+
+  const handleDeleteRecord = (index) => {
+    const updatedCharacters = [...data];
+    updatedCharacters.splice(index, 1);
+    setData(updatedCharacters);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <button onClick={handleAddRecord}>Add Record</button>
+      <table id="characterTable">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Height</th>
+            <th>Mass</th>
+            <th>Gender</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((character, index) => (
+            <tr key={index}>
+              <td>{character.name}</td>
+              <td>{character.height}</td>
+              <td>{character.mass}</td>
+              <td>{character.gender}</td>
+              <td><button onClick={() => handleDeleteRecord(index)}>Delete</button></td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
